07d7a274ac97c5b30da1ae25af51608a2d1b5233
kh8514 <47620827+kh8514@users.noreply.github.com>
2022-06-21 17:10:33 +0900

    네비바

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.esm.min.js'
+
+
+const app = createApp(App)
+
+app.mount('#app')
