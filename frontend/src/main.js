5e186536db765b4178c87ae46996eb0dfc8793f0
kh8514 <47620827+kh8514@users.noreply.github.com>
2022-06-22 10:49:25 +0900

    db

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,9 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.esm.min.js'
 
+import { store } from '@/store/index';
 
 const app = createApp(App)
-
+app.use(store)
 app.mount('#app')
+
